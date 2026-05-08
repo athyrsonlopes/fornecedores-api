9d39f299708e1f4f269c7e1761c8405fd9b8c66a
Athyrson Lopes <athyrson.ea@gmail.com>
2026-05-08 00:01:59 -0300

    feat: adiciona validadores no model

--- app/models/fornecedor.rb
+++ app/models/fornecedor.rb
@@ -1,2 +1,6 @@
 class Fornecedor < ApplicationRecord
+  validates :nome, presence: true
+  validates :cnpj, presence: true, uniqueness: true
+  validates :categoria, presence: true
+  validates :status, presence: true, inclusion: { in: %w[ativo inativo] }
 end
